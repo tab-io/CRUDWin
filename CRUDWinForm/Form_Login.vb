006da55b38f0bba3899e6366d806a2999093b23c
Michael-Vigil <39469706+Michael-Vigil@users.noreply.github.com>
2021-05-25 22:10:11 -0400

    Realized I created this project in a rush and had the incorrect .Net Framework Version. It is now 4.8.
    Added the user class with user information properties that are in-line
    with the user table.

--- CRUDWinForm/Form_Login.vb
+++ CRUDWinForm/Form_Login.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form_Login
+    Private Sub Button_Login_Click(sender As Object, e As EventArgs) Handles Button_Login.Click
+
+    End Sub
 
 End Class
---
+++ CRUDWinForm/User.vb
@@ -0,0 +1,46 @@
+﻿Public Class User
+
+    Private _username As String
+    Private _password As String
+    Private _firstName As String
+    Private _lastName As String
+    Private _phoneNumber As String
+    Private _emailAddress As String
+
+    Public ReadOnly Property UserName() As String
+        Get
+            Return _username
+        End Get
+    End Property
+
+    Public ReadOnly Property Password() As String
+        Get
+            Return _password
+        End Get
+    End Property
+
+    Public ReadOnly Property FirstName() As String
+        Get
+            Return _firstName
+        End Get
+    End Property
+
+    Public ReadOnly Property LastName() As String
+        Get
+            Return _lastName
+        End Get
+    End Property
+
+    Public ReadOnly Property PhoneNumber() As String
+        Get
+            Return _phoneNumber
+        End Get
+    End Property
+
+    Public ReadOnly Property EmailAddress() As String
+        Get
+            Return _emailAddress
+        End Get
+    End Property
+
+End Class
